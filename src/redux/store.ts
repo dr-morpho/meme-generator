57495da17a2cf48d4f3ef2c3bad828f756e0a3c5
Rodion Minaev <102305155+dr-morpho@users.noreply.github.com>
2022-07-20 09:15:16 +0300

    Update store.ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -22,6 +22,7 @@ const rootReducer = combineReducers({
 const persistConfig = {
   key: 'root',
   storage,
+  blacklist: ['memeSlice'],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
